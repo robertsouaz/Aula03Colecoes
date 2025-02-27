1f99f56caa755dcbfbaabcd8c79a618060934455
Roberto Dias <robertsouaz@hotmail.com>
2025-02-26 21:15:26 -0300

    first commit

---
+++ Models/Funcionario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula03Colecoes.Models
+{
+    public class Funcionarios
+    {
+        
+    }
+}
