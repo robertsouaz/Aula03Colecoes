1c2343a52352cb61767f3b6930dd8c425035d162
Roberto Dias <robertsouaz@hotmail.com>
2025-02-26 22:44:16 -0300

    first commit

--- Models/Enuns/TipoFuncionariosEnum.cs
+++ Models/Enuns/TipoFuncionariosEnum.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Aula03Colecoes.Models.Enuns
 {
-    public class TipoFuncionariosEnum
-    {
-        
-    }
+public enum TipoFuncionariosEnum
+{
+   CLT = 1,
+   Aprendiz = 2
+}
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,132 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+using System.ComponentModel;
+using Aula03Colecoes.Models;
+ using Aula03Colecoes.Models.Enuns;
+ namespace Aula03Colecoes
+ {
+     public class Program
+     {
+        
+        static List<Funcionario> lista = new List<Funcionario>();
+        static void Main(string[] args)
+        {
+            CriarLista();
+            // ExibirLista();
+            ObterPorId();
+            ExemplosListasColecoes();
+        }
+
+        public static void ExemplosListasColecoes()
+        {
+        Console.WriteLine("==================================================");  
+        Console.WriteLine("****** Exemplos - Aula 03 Listas e Coleções ******");  
+        Console.WriteLine("==================================================");  
+        CriarLista();
+        int opcaoEscolhida = 0;
+        do
+        {
+        Console.WriteLine("=================================================="); 
+        Console.WriteLine("---Digite o número referente a opção desejada: ---");  
+        Console.WriteLine("1 - Obter Por Id");
+        Console.WriteLine("==================================================");  
+        Console.WriteLine("-----Ou tecle qualquer outro número para sair-----");  
+        Console.WriteLine("==================================================");  
+        opcaoEscolhida = int.Parse(Console.ReadLine());
+        string mensagem = string.Empty;
+        switch (opcaoEscolhida)
+        {
+        case 1:
+        ObterPorId();
+        break;
+        default:
+        Console.WriteLine("Saindo do sistema....");
+        break;
+        }
+        } while (opcaoEscolhida >= 1 && opcaoEscolhida <= 10);
+        Console.WriteLine("==================================================");  
+        Console.WriteLine("* Obrigado por utilizar o sistema e volte sempre *");  
+        Console.WriteLine("==================================================");  
+        }
+
+        public static void ObterPorId()
+        {
+            lista = lista.FindAll(x => x.Id > 0);
+            ExibirLista();
+        }
+
+        public static void ExibirLista()
+        {
+            string dados = "";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                dados += "===============================\n";
+                dados += string.Format("Id: {0} \n", lista[i].Id);
+                dados += string.Format("Nome: {0} \n", lista[i].Nome);
+                dados += string.Format("CPF: {0} \n", lista[i].Cpf);
+                dados += string.Format("Admissão: {0:dd/MM/yyyy} \n", lista[i].DataAdmissao);
+                dados += string.Format("Salário: {0:c2} \n", lista[i].Salario);
+                dados += string.Format("Tipo: {0} \n", lista[i].TipoFuncionarios);
+                dados += "===============================\n";                
+            }
+            Console.WriteLine(dados);
+        }
+    
+    public static void CriarLista()
+        {
+            Funcionario f1 = new Funcionario();
+            f1.Id = 1;
+            f1.Nome = "Neymar";
+            f1.Cpf = "12345678910";
+            f1.DataAdmissao = DateTime.Parse("01/01/2000");
+            f1.Salario = 150.000M;
+            f1.TipoFuncionarios = TipoFuncionariosEnum.CLT;
+            lista.Add(f1);
+
+            Funcionario f2 = new Funcionario();
+            f2.Id = 2;
+            f2.Nome = "Cristiano Ronaldo";
+            f2.Cpf = "01987654321";
+            f2.DataAdmissao = DateTime.Parse("30/06/2002");
+            f2.Salario = 150.000M;
+            f2.TipoFuncionarios = TipoFuncionariosEnum.CLT;
+            lista.Add(f2);
+
+            Funcionario f3 = new Funcionario();
+            f3.Id = 3;
+            f3.Nome = "Messi";
+            f3.Cpf = "135792468";
+            f3.DataAdmissao = DateTime.Parse("01/11/2003");
+            f3.Salario = 70.000M;
+            f3.TipoFuncionarios = TipoFuncionariosEnum.Aprendiz;
+            lista.Add(f3);
+
+            Funcionario f4 = new Funcionario();
+            f4.Id = 4;
+            f4.Nome = "Mbappe";
+            f4.Cpf = "246813579";
+            f4.DataAdmissao = DateTime.Parse("15/09/2005");
+            f4.Salario = 80.000M;
+            f4.TipoFuncionarios = TipoFuncionariosEnum.Aprendiz;
+            lista.Add(f4);
+            
+            Funcionario f5 = new Funcionario();
+            f5.Id = 5;
+            f5.Nome = "Lewa";
+            f5.Cpf = "246813579";
+            f5.DataAdmissao = DateTime.Parse("20/10/1998");
+            f5.Salario = 90.000M;
+            f5.TipoFuncionarios = TipoFuncionariosEnum.Aprendiz;
+            lista.Add(f5);
+
+            Funcionario f6 = new Funcionario();
+            f6.Id = 6;
+            f6.Nome = "Rodrigo Garro";
+            f6.Cpf = "246813579";
+            f6.DataAdmissao = DateTime.Parse("13/12/1997");
+            f6.Salario = 300.000M;
+            f6.TipoFuncionarios = TipoFuncionariosEnum.CLT;
+            lista.Add(f6);
+        }
+            
+ }
+}
